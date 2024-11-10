6fcbfe8adf7dd39899b36fbedac5e4809991d419
Niilo Heinonen <heinonen.niilo@gmail.com>
2024-11-10 23:23:37 +0200

    Cleaning

--- src/EnvironmentMonitor.Application/Extensions/ServiceCollectionExtensions.cs
+++ src/EnvironmentMonitor.Application/Extensions/ServiceCollectionExtensions.cs
@@ -2,11 +2,6 @@
 using EnvironmentMonitor.Application.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EnvironmentMonitor.Application.Extensions
 {
@@ -14,24 +9,8 @@ namespace EnvironmentMonitor.Application.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
         {
-            // Configure DbContext
             services.AddScoped<IMeasurementService, MeasurementService>();
-            // Add other services as needed
             return services;
         }
     }
 }
-
-/*
-    public static class ServiceCollectionExtensions
-    {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
-        {
-            // Configure DbContext
-            services.AddInfrastructureServices(configuration);
-            // Add other services as needed
-            return services;
-        }
-    }
-/
-*/
