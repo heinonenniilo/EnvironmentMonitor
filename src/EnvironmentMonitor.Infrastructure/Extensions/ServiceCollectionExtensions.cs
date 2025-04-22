381a544d2d980bb0369caae798e09b768684b7f4
Niilo Heinonen <heinonen.niilo@gmail.com>
2025-04-22 18:30:12 +0300

    Set StorageClient & HubMessageService as singletons

--- src/EnvironmentMonitor.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/EnvironmentMonitor.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -17,8 +17,8 @@ namespace EnvironmentMonitor.Infrastructure.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, 
-            IConfiguration configuration, 
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+            IConfiguration configuration,
             string? connectionString = null
             )
         {
@@ -45,17 +45,17 @@ namespace EnvironmentMonitor.Infrastructure.Extensions
                 .AddRoleManager<RoleManager<ApplicationUserRole>>();
             services.AddScoped<IRoleManager, RoleManager>();
             services.AddScoped<IUserAuthService, UserAuthService>();
-            
+
 
             var defaultSettings = new IotHubSettings();
             configuration.GetSection("IotHubSettings").Bind(defaultSettings);
-            var storageAccountSettings = new StorageAccountSettings() { AccountName = "", ContainerName = ""};
+            var storageAccountSettings = new StorageAccountSettings() { AccountName = "", ContainerName = "" };
             configuration.GetSection("StorageSettings").Bind(storageAccountSettings);
             services.AddSingleton(defaultSettings);
             services.AddSingleton(storageAccountSettings);
             services.AddSingleton<IDateService, DateService>();
-            services.AddScoped<IHubMessageService, HubMessageService>();
-            services.AddScoped<IStorageClient, StorageClient>();
+            services.AddSingleton<IHubMessageService, HubMessageService>();
+            services.AddSingleton<IStorageClient, StorageClient>();
             return services;
         }
     }
