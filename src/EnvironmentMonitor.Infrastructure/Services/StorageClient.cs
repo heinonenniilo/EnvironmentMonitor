2008c4a225d46b76ceab52a80b8343a14f663dd3
Niilo Heinonen <heinonen.niilo@gmail.com>
2025-04-22 18:26:14 +0300

    Changes for storage client

--- src/EnvironmentMonitor.Infrastructure/Services/StorageClient.cs
+++ src/EnvironmentMonitor.Infrastructure/Services/StorageClient.cs
@@ -2,6 +2,7 @@
 using Azure.Storage.Blobs;
 using EnvironmentMonitor.Domain.Interfaces;
 using EnvironmentMonitor.Domain.Models;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,25 +14,49 @@ namespace EnvironmentMonitor.Infrastructure.Services
 {
     public class StorageClient : IStorageClient
     {
-        private readonly BlobContainerClient _containerClient;
+        private readonly BlobContainerClient? _containerClient;
         private readonly StorageAccountSettings _settings;
-        public StorageClient(StorageAccountSettings settings)
+        private readonly ILogger<StorageClient> _logger;
+        public StorageClient(StorageAccountSettings settings, ILogger<StorageClient> logger)
         {
-            var credential = new DefaultAzureCredential(); // uses system-assigned managed identity
+            var credential = new DefaultAzureCredential();
+            _logger = logger;
             _settings = settings;
 
-            _containerClient = !string.IsNullOrEmpty(_settings.ConnectionString) ?
-                new BlobContainerClient(_settings.ConnectionString, _settings.ContainerName,
-                new BlobClientOptions()) : new BlobContainerClient(new Uri(_settings.AccountUri), new DefaultAzureCredential());
-            _containerClient.CreateIfNotExists();
+            if (!string.IsNullOrEmpty(_settings.ConnectionString))
+            {
+                _containerClient = new BlobContainerClient(_settings.ConnectionString, _settings.ContainerName,
+                new BlobClientOptions());
+            }
+            else if (!string.IsNullOrEmpty(_settings.AccountUri))
+            {
+                _logger.LogInformation($"Trying to form blob container client with uri: {_settings.AccountUri}. ");
+                _containerClient = new BlobContainerClient(new Uri(_settings.AccountUri), new DefaultAzureCredential());
+            }
+            else if (!string.IsNullOrEmpty(_settings.AccountName) && !string.IsNullOrEmpty(_settings.ContainerName))
+            {
+                var uriString = $"https://{_settings.AccountName}.blob.core.windows.net/{_settings.ContainerName}";
+                _logger.LogInformation($"Trying to form blob container client with uri: {uriString}. ");
+                _containerClient = new BlobContainerClient(new Uri(uriString), new DefaultAzureCredential());
+            }
+            else
+            {
+                _logger.LogWarning("No setting defined for storage client");
+            }
         }
 
         public async Task<Uri> Upload(Stream stream, string blobName)
         {
+            if (_containerClient == null)
+            {
+                throw new InvalidOperationException("Blob client not initialized");
+            }
+            await _containerClient.CreateIfNotExistsAsync();
             if (string.IsNullOrEmpty(blobName))
             {
                 throw new ArgumentException("Invalid blob name");
             }
+            _logger.LogInformation($"Trying to upload blob named: {blobName}");
             var blobClient = _containerClient.GetBlobClient(blobName);
             var res = await blobClient.UploadAsync(stream, overwrite: true);
             return blobClient.Uri;
@@ -39,6 +64,10 @@ namespace EnvironmentMonitor.Infrastructure.Services
 
         public async Task<AttachmentInfoModel> GetImageAsync(string fileName)
         {
+            if (_containerClient == null)
+            {
+                throw new InvalidOperationException("No container client initialized");
+            }
             var blobClient = _containerClient.GetBlobClient(fileName);
             var exists = await blobClient.ExistsAsync();
             if (!exists)
