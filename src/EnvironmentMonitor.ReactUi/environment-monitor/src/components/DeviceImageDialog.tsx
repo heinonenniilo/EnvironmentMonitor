70ed874f363bd85bb168fd2f409c2037d22e1116
Niilo Heinonen <heinonen.niilo@gmail.com>
2025-04-27 13:02:43 +0300

    Hide title for small screens

--- src/EnvironmentMonitor.ReactUi/environment-monitor/src/components/DeviceImageDialog.tsx
+++ src/EnvironmentMonitor.ReactUi/environment-monitor/src/components/DeviceImageDialog.tsx
@@ -5,6 +5,8 @@ import {
   DialogContent,
   DialogTitle,
   IconButton,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 
 export interface DeviceImageDialogProps {
@@ -20,31 +22,37 @@ export const DeviceImageDialog: React.FC<DeviceImageDialogProps> = ({
   isOpen,
   onClose,
 }) => {
+  const theme = useTheme();
+  const drawTitle = useMediaQuery(theme.breakpoints.up("lg"));
+
   return (
     <Dialog open={isOpen} onClose={onClose} maxWidth="xl">
-      <DialogTitle
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <Box>{title ?? ""}</Box>
-        <Box sx={{ display: "flex", flexBasis: "row" }}>
-          <IconButton
-            aria-label="close"
-            onClick={() => {
-              onClose();
-            }}
-            sx={{
-              color: (theme) => theme.palette.grey[500],
-            }}
-            size="small"
-          >
-            <Close />
-          </IconButton>
-        </Box>
-      </DialogTitle>
+      {drawTitle && (
+        <DialogTitle
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box>{title ?? ""}</Box>
+          <Box sx={{ display: "flex", flexBasis: "row" }}>
+            <IconButton
+              aria-label="close"
+              onClick={() => {
+                onClose();
+              }}
+              sx={{
+                color: (theme) => theme.palette.grey[500],
+              }}
+              size="small"
+            >
+              <Close />
+            </IconButton>
+          </Box>
+        </DialogTitle>
+      )}
+
       <DialogContent>
         <Box
           component="img"
