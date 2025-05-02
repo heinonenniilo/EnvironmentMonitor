36f48c3fdf03a1fa4863ffef53f79d01e3ec6cf4
Niilo Heinonen <heinonen.niilo@gmail.com>
2025-05-03 01:20:51 +0300

    Migrations

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20250502220231_AddDeviceMessages.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeviceMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DeviceMessageId",
+                table: "Measurements",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeviceMessageId",
+                table: "DeviceStatusChanges",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "DeviceMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DeviceId = table.Column<int>(type: "int", nullable: false),
+                    TimeStamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TimeStampUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceMessages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DeviceMessages_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Measurements_DeviceMessageId",
+                table: "Measurements",
+                column: "DeviceMessageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceStatusChanges_DeviceMessageId",
+                table: "DeviceStatusChanges",
+                column: "DeviceMessageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceMessages_DeviceId",
+                table: "DeviceMessages",
+                column: "DeviceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DeviceStatusChanges_DeviceMessages_DeviceMessageId",
+                table: "DeviceStatusChanges",
+                column: "DeviceMessageId",
+                principalTable: "DeviceMessages",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Measurements_DeviceMessages_DeviceMessageId",
+                table: "Measurements",
+                column: "DeviceMessageId",
+                principalTable: "DeviceMessages",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DeviceStatusChanges_DeviceMessages_DeviceMessageId",
+                table: "DeviceStatusChanges");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Measurements_DeviceMessages_DeviceMessageId",
+                table: "Measurements");
+
+            migrationBuilder.DropTable(
+                name: "DeviceMessages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Measurements_DeviceMessageId",
+                table: "Measurements");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DeviceStatusChanges_DeviceMessageId",
+                table: "DeviceStatusChanges");
+
+            migrationBuilder.DropColumn(
+                name: "DeviceMessageId",
+                table: "Measurements");
+
+            migrationBuilder.DropColumn(
+                name: "DeviceMessageId",
+                table: "DeviceStatusChanges");
+        }
+    }
+}
