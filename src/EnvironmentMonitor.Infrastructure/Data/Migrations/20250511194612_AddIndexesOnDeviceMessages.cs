7e43ce2f458c44e3c431604a9978aea8230365ae
Niilo Heinonen <heinonen.niilo@gmail.com>
2025-05-11 22:48:43 +0300

    Migrations

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20250511194612_AddIndexesOnDeviceMessages.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexesOnDeviceMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DeviceMessages_DeviceId",
+                table: "DeviceMessages");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceMessages_DeviceId_TimeStamp",
+                table: "DeviceMessages",
+                columns: new[] { "DeviceId", "TimeStamp" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceMessages_TimeStamp",
+                table: "DeviceMessages",
+                column: "TimeStamp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DeviceMessages_DeviceId_TimeStamp",
+                table: "DeviceMessages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DeviceMessages_TimeStamp",
+                table: "DeviceMessages");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceMessages_DeviceId",
+                table: "DeviceMessages",
+                column: "DeviceId");
+        }
+    }
+}
