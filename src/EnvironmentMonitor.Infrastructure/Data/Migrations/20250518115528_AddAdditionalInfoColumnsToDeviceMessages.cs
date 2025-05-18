54f2fc7e50fb2bce73b994fda3a6f966feb6500d
Niilo Heinonen <heinonen.niilo@gmail.com>
2025-05-18 15:26:56 +0300

    Migrations

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20250518115528_AddAdditionalInfoColumnsToDeviceMessages.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdditionalInfoColumnsToDeviceMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Identifier",
+                table: "DeviceMessages",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "LoopCount",
+                table: "DeviceMessages",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "MessageCount",
+                table: "DeviceMessages",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "Uptime",
+                table: "DeviceMessages",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Identifier",
+                table: "DeviceMessages");
+
+            migrationBuilder.DropColumn(
+                name: "LoopCount",
+                table: "DeviceMessages");
+
+            migrationBuilder.DropColumn(
+                name: "MessageCount",
+                table: "DeviceMessages");
+
+            migrationBuilder.DropColumn(
+                name: "Uptime",
+                table: "DeviceMessages");
+        }
+    }
+}
