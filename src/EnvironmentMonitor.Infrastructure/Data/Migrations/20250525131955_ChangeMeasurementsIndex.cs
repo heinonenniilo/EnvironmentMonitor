46afa51cfde640679d4027eff0e7b6aab238521c
Niilo Heinonen <heinonen.niilo@gmail.com>
2025-05-25 16:21:10 +0300

    Migration

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20250525131955_ChangeMeasurementsIndex.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeMeasurementsIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Measurements_SensorId_Timestamp",
+                table: "Measurements");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Measurements_SensorId_Timestamp",
+                table: "Measurements",
+                columns: new[] { "SensorId", "Timestamp" })
+                .Annotation("SqlServer:Include", new[] { "Value", "TypeId", "TimestampUtc" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Measurements_SensorId_Timestamp",
+                table: "Measurements");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Measurements_SensorId_Timestamp",
+                table: "Measurements",
+                columns: new[] { "SensorId", "Timestamp" });
+        }
+    }
+}
