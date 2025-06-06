c1164dee0e93a661575717c1a1b90b6b7c3565e1
Niilo Heinonen <heinonen.niilo@gmail.com>
2025-06-07 00:13:16 +0300

    Fix for timerange handling

--- src/EnvironmentMonitor.Infrastructure/Data/DeviceRepository.cs
+++ src/EnvironmentMonitor.Infrastructure/Data/DeviceRepository.cs
@@ -212,7 +212,12 @@ namespace EnvironmentMonitor.Infrastructure.Data
                 var latestStatusBeforeTimeRangeStart = await _context.DeviceStatusChanges.Where(x => x.DeviceId == deviceId && x.TimeStamp < model.From).OrderByDescending(x => x.TimeStamp).FirstOrDefaultAsync();
                 if (latestStatusBeforeTimeRangeStart != null)
                 {
-                    listToReturn.Add(latestStatusBeforeTimeRangeStart);
+                    listToReturn.Add(new DeviceStatus()
+                    {
+                        DeviceId = latestStatusBeforeTimeRangeStart.DeviceId,
+                        TimeStamp = model.From,
+                        Status = latestStatusBeforeTimeRangeStart.Status,
+                    });
                 }
             }
 
