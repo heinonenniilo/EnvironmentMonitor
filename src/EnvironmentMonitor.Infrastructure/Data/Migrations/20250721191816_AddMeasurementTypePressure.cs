d4c684f70268caf0012dcf6e480ed3022d42bda2
heinonen.niilo@gmail.com <heinonen.niilo@gmail.com>
2025-07-21 22:21:16 +0300

    Migrations

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20250721191816_AddMeasurementTypePressure.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMeasurementTypePressure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "MeasurementTypes",
+                columns: new[] { "Id", "Name", "Unit" },
+                values: new object[] { 5, "Pressure", "-" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MeasurementTypes",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
