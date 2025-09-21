4c087edd7bf3ab850eec1fc529f5d253e95c6376
Niilo Heinonen <heinonen.niilo@gmail.com>
2025-09-21 19:50:28 +0300

    Migrations

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20250921155431_AddPublicSensors.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPublicSensors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PublicSensors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SensorId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: false),
+                    TypeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PublicSensors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PublicSensors_MeasurementTypes_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "MeasurementTypes",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_PublicSensors_Sensors_SensorId",
+                        column: x => x.SensorId,
+                        principalTable: "Sensors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PublicSensors_SensorId",
+                table: "PublicSensors",
+                column: "SensorId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PublicSensors_TypeId",
+                table: "PublicSensors",
+                column: "TypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PublicSensors");
+        }
+    }
+}
