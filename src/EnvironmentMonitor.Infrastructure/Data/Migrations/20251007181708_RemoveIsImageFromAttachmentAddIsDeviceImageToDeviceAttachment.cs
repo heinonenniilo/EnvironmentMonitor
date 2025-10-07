cc45715157350cda7d2b35083ae85f1fdc1a38a3
heinonenniilo <heinonen.niilo@gmail.com>
2025-10-07 21:54:49 +0300

    Added migrations

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20251007181708_RemoveIsImageFromAttachmentAddIsDeviceImageToDeviceAttachment.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIsImageFromAttachmentAddIsDeviceImageToDeviceAttachment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsImage",
+                table: "Attachments");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeviceImage",
+                table: "DeviceAttachments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+
+            migrationBuilder.Sql(@"
+update da
+set 
+    IsDeviceImage = 1
+from 
+    DeviceAttachments da
+join 
+    Attachments a on a.Id = da.AttachmentId
+where 
+    a.ContentType like '%image%'
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeviceImage",
+                table: "DeviceAttachments");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsImage",
+                table: "Attachments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
