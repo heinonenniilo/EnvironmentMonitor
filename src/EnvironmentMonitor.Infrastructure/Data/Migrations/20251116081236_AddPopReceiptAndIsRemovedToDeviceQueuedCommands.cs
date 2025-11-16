c1c1667b9dcb8d3e133d5a83d70a481efdd14fd4
heinonenniilo <heinonen.niilo@gmail.com>
2025-11-16 10:42:28 +0200

    migrations, PopReceipt column

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20251116081236_AddPopReceiptAndIsRemovedToDeviceQueuedCommands.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPopReceiptAndIsRemovedToDeviceQueuedCommands : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRemoved",
+                table: "DeviceQueuedCommands",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PopReceipt",
+                table: "DeviceQueuedCommands",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRemoved",
+                table: "DeviceQueuedCommands");
+
+            migrationBuilder.DropColumn(
+                name: "PopReceipt",
+                table: "DeviceQueuedCommands");
+        }
+    }
+}
