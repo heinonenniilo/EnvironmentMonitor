6e98220b26c43d03105225d9800bc904fd74f25f
heinonenniilo <heinonen.niilo@gmail.com>
2025-11-30 11:00:05 +0200

    Removed DeviceIdentifier from SensorDto and renamed it as ParentIdentifier

--- src/EnvironmentMonitor.Application/DTOs/SensorDto.cs
+++ src/EnvironmentMonitor.Application/DTOs/SensorDto.cs
@@ -14,26 +14,27 @@ namespace EnvironmentMonitor.Application.DTOs
     public class SensorDto : EntityDto, IMapFrom<Sensor>, IMapFrom<SensorExtended>
     {
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public Guid? DeviceIdentifier { get; set; }
+        public Guid? ParentIdentifier { get; set; }
         public double? ScaleMin { get; set; }
         public double? ScaleMax { get; set; }
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public Guid? LocationIdentifier { get; set; }
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public int? MeasurementType { get; set; }
 
         public virtual void Mapping(Profile profile)
         {
             profile.CreateMap<Sensor, SensorDto>()
-                .ForMember(x => x.DeviceIdentifier, opt => opt.MapFrom(d => d.Device != null ? d.Device.Identifier : (Guid?)null))
+                .ForMember(x => x.ParentIdentifier, opt => opt.MapFrom(d => d.Device != null ? d.Device.Identifier : (Guid?)null))
                 .ReverseMap();
-            profile.CreateMap<SensorExtended, SensorDto>().ReverseMap();
+
+            profile.CreateMap<SensorExtended, SensorDto>()
+                .ForMember(x => x.ParentIdentifier, opt => opt.MapFrom(x => x.DeviceIdentifier))
+                .ReverseMap();
 
             profile.CreateMap<LocationSensor, SensorDto>()
                 .ForMember(x => x.Identifier, opt => opt.MapFrom(x => x.Sensor.Identifier))
                 .ForMember(x => x.ScaleMin, opt => opt.MapFrom(x => x.Sensor != null ? x.Sensor.ScaleMin : null))
                 .ForMember(x => x.ScaleMax, opt => opt.MapFrom(x => x.Sensor != null ? x.Sensor.ScaleMax : null))
-                .ForMember(x => x.LocationIdentifier, opt => opt.MapFrom(x => x.Location != null ? x.Location.Identifier : (Guid?)null))
+                .ForMember(x => x.ParentIdentifier, opt => opt.MapFrom(x => x.Location != null ? x.Location.Identifier : (Guid?)null))
                 .ForMember(x => x.MeasurementType, opt => opt.MapFrom(x => x.TypeId))
                 .ReverseMap();
             profile.CreateMap<PublicSensor, SensorDto>()
