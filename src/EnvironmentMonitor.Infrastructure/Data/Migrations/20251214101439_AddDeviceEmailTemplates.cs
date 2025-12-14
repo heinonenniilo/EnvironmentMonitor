6eab00bea5e9b4156f9f0097414e2f2c591ffc2d
heinonenniilo <heinonen.niilo@gmail.com>
2025-12-14 13:13:54 +0200

    Migration

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20251214101439_AddDeviceEmailTemplates.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeviceEmailTemplates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeviceEmailTemplates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: true),
+                    Message = table.Column<string>(type: "nvarchar(max)", maxLength: 256, nullable: true),
+                    CreatedUtc = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Updated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceEmailTemplates", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "DeviceEmailTemplates",
+                columns: new[] { "Id", "Created", "Message", "Title", "Updated" },
+                values: new object[,]
+                {
+                    { 0, null, null, null, null },
+                    { 1, null, null, null, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeviceEmailTemplates");
+        }
+    }
+}
