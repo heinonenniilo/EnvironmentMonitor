8a10dd1632b57b6e9b8fa38291ba8d0c14ad3c36
heinonenniilo <heinonen.niilo@gmail.com>
2025-12-20 01:07:52 +0200

    Migrations

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20251219225438_AddTrackedEntityUpdated.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrackedEntityUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Updated",
+                table: "DeviceQueuedCommands",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedUtc",
+                table: "DeviceQueuedCommands",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Updated",
+                table: "DeviceMessages",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedUtc",
+                table: "DeviceMessages",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Updated",
+                table: "DeviceContacts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedUtc",
+                table: "DeviceContacts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Updated",
+                table: "DeviceAttributes",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedUtc",
+                table: "DeviceAttributes",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Updated",
+                table: "DeviceAttachments",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedUtc",
+                table: "DeviceAttachments",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Updated",
+                table: "Attachments",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedUtc",
+                table: "Attachments",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "DeviceQueuedCommands");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedUtc",
+                table: "DeviceQueuedCommands");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "DeviceMessages");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedUtc",
+                table: "DeviceMessages");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "DeviceContacts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedUtc",
+                table: "DeviceContacts");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "DeviceAttributes");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedUtc",
+                table: "DeviceAttributes");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "DeviceAttachments");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedUtc",
+                table: "DeviceAttachments");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "Attachments");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedUtc",
+                table: "Attachments");
+        }
+    }
+}
