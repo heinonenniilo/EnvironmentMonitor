80b282c996847ac63bb2248c719383c7dd5b2844
heinonenniilo <heinonen.niilo@gmail.com>
2025-12-20 00:14:57 +0200

    Migrations

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20251219215715_AddDeviceContacts.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeviceContacts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeviceContacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Identifier = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    DeviceId = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false),
+                    CreatedUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceContacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DeviceContacts_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceContacts_DeviceId_Email",
+                table: "DeviceContacts",
+                columns: new[] { "DeviceId", "Email" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceContacts_Identifier",
+                table: "DeviceContacts",
+                column: "Identifier",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeviceContacts");
+        }
+    }
+}
