3c3b141dd489b2d28a083643b2d820236879897b
heinonenniilo <heinonen.niilo@gmail.com>
2025-12-23 02:33:51 +0200

    Added displayName

--- src/EnvironmentMonitor.Application/DTOs/DeviceEmailTemplateDto.cs
+++ src/EnvironmentMonitor.Application/DTOs/DeviceEmailTemplateDto.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using EnvironmentMonitor.Application.Mappings;
 using EnvironmentMonitor.Domain.Entities;
+using EnvironmentMonitor.Domain.Enums;
 
 namespace EnvironmentMonitor.Application.DTOs
 {
@@ -12,11 +13,12 @@ namespace EnvironmentMonitor.Application.DTOs
         public DateTime CreatedUtc { get; set; }
         public DateTime? Created { get; set; }
         public DateTime? Updated { get; set; }
+        public string DisplayName { get; set; } = string.Empty;
 
         public void Mapping(Profile profile)
         {
             profile.CreateMap<DeviceEmailTemplate, DeviceEmailTemplateDto>()
-                .ReverseMap();
+                .ForMember(x => x.DisplayName, opt => opt.MapFrom(x => ((DeviceEmailTemplateTypes)x.Id).ToString()));
         }
     }
 }
