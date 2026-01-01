d82feb1cb015868591225ace0c637ea070c588e6
heinonenniilo <niilo.heinonen@proton.me>
2026-01-02 00:35:13 +0200

    Formatting improvements

--- src/EnvironmentMonitor.Domain/Utils/EmailUtils.cs
+++ src/EnvironmentMonitor.Domain/Utils/EmailUtils.cs
@@ -10,16 +10,24 @@ namespace EnvironmentMonitor.Domain.Utils
         public static void ReplaceTokens(SendEmailOptions options)
         {
             if (options.ReplaceTokens == null || !options.ReplaceTokens.Any())
+            {
                 return;
+            }
 
             foreach (var token in options.ReplaceTokens)
             {
                 if (!string.IsNullOrEmpty(options.Subject))
+                {
                     options.Subject = options.Subject.Replace(token.Key, token.Value);
+                }
                 if (!string.IsNullOrEmpty(options.HtmlContent))
+                {
                     options.HtmlContent = options.HtmlContent.Replace(token.Key, token.Value);
+                }
                 if (!string.IsNullOrEmpty(options.PlainTextContent))
+                {
                     options.PlainTextContent = options.PlainTextContent.Replace(token.Key, token.Value);
+                }
             }
         }
 
@@ -28,11 +36,16 @@ namespace EnvironmentMonitor.Domain.Utils
             var addresses = new List<string>();
             foreach (var source in addressSources)
             {
-                if (source == null) continue;
+                if (source == null) 
+                { 
+                    continue; 
+                }
                 if (source is string singleAddress)
                 {
                     if (!string.IsNullOrWhiteSpace(singleAddress))
+                    {
                         addresses.Add(singleAddress);
+                    }
                 }
                 else if (source is IEnumerable<string> addressList)
                 {
