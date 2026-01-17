ab04300c6fe1aa4772b2554de9e254c612dcaf65
heinonenniilo <niilo.heinonen@proton.me>
2026-01-18 00:28:14 +0200

    Migrations

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20260117221635_AddASensorsggregationType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddASensorsggregationType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AggregationType",
+                table: "Sensors",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AggregationType",
+                table: "Sensors");
+        }
+    }
+}
