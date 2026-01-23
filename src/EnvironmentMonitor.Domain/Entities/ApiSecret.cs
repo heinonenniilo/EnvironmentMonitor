e4b977b24aafbba979c143e6328a55cfa3fd2924
heinonenniilo <niilo.heinonen@proton.me>
2026-01-24 01:11:53 +0200

    Added enabled

--- src/EnvironmentMonitor.Domain/Entities/ApiSecret.cs
+++ src/EnvironmentMonitor.Domain/Entities/ApiSecret.cs
@@ -8,6 +8,7 @@ namespace EnvironmentMonitor.Domain.Entities
         public required string Id { get; set; }
         public byte[] Hash { get; set; } = Array.Empty<byte>();
         public string? Description { get; set; }
+        public bool Enabled { get; set; } = true;
         public ICollection<SecretClaim> Claims { get; set; } = new List<SecretClaim>();
     }
 }
