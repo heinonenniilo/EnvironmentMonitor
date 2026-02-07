317feb28e2b37c3b375ed9ef123191eed88fb1d1
heinonenniilo <niilo.heinonen@proton.me>
2026-02-07 02:00:59 +0200

    Migrations

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20260206225444_AddSensorsIsActive.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSensorsIsActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Sensors",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Sensors");
+        }
+    }
+}
