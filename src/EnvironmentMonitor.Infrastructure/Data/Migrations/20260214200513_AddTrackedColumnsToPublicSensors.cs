18ff8b783d431b264c02ee6fffff03452dfaabe7
heinonenniilo <niilo.heinonen@proton.me>
2026-02-14 22:35:10 +0200

    Migrations

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20260214200513_AddTrackedColumnsToPublicSensors.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrackedColumnsToPublicSensors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "PublicSensors",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedUtc",
+                table: "PublicSensors",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Updated",
+                table: "PublicSensors",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedUtc",
+                table: "PublicSensors",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "PublicSensors");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedUtc",
+                table: "PublicSensors");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "PublicSensors");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedUtc",
+                table: "PublicSensors");
+        }
+    }
+}
