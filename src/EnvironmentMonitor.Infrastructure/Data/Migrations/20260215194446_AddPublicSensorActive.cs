83d5bac1b330d4e599ca09bf9ca9a9c4fd164832
heinonenniilo <niilo.heinonen@proton.me>
2026-02-15 21:59:13 +0200

    Added Active to sensors

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20260215194446_AddPublicSensorActive.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPublicSensorActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "PublicSensors",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.Sql("UPDATE PublicSensors SET Active = 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "PublicSensors");
+        }
+    }
+}
