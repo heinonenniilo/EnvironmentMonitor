0904095d113269a9bd570446297a9a1e1cabcb9c
heinonenniilo <niilo.heinonen@proton.me>
2026-02-22 12:27:16 +0200

    Revert "In ApiKeyAuthenticationTests, clear cache in setup phase"
    This reverts commit 473d6e64e9babf7a28c1219395877ce6232fdd74.

--- tests/EnvironmentMonitor.Tests/ApiKeyAuthenticationTests.cs
+++ tests/EnvironmentMonitor.Tests/ApiKeyAuthenticationTests.cs
@@ -26,10 +26,6 @@ namespace EnvironmentMonitor.Tests
             using var scope = _factory.Services.CreateScope();
             var configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
             _apiKey = configuration.GetSection("ApiKey").Value;
-
-            var cacheService = scope.ServiceProvider.GetRequiredService<ICacheService>();
-            await cacheService.ClearAsync();
-
             await LogoutAsync();
         }
 
