89879eaf82056a13a1f78b81b217cdd935da68ab
heinonenniilo <niilo.heinonen@proton.me>
2026-04-03 12:44:58 +0300

    Migrations

---
+++ src/EnvironmentMonitor.Infrastructure/Data/Migrations/20260403090207_AddLongitudeAndLatitudeToPublicSensor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvironmentMonitor.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLongitudeAndLatitudeToPublicSensor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "PublicSensors",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "PublicSensors",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "PublicSensors");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "PublicSensors");
+        }
+    }
+}
