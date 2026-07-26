e59f070638cf15cf7e32e0f0adb1db6049c69677
heinonenniilo <niilo.heinonen@proton.me>
2026-07-26 22:37:28 +0300

    Logic and logging improvements for DateService

--- src/EnvironmentMonitor.Infrastructure/Services/DateService.cs
+++ src/EnvironmentMonitor.Infrastructure/Services/DateService.cs
@@ -15,17 +15,24 @@ namespace EnvironmentMonitor.Infrastructure.Services
         private readonly ILogger<DateService> _logger;
         private readonly ApplicationSettings _applicationSettings;
 
+        private TimeZoneInfo? _localTimeZone { get; set; }
+
         public DateService(ILogger<DateService> logger, ApplicationSettings applicationSettings)
         {
             _logger = logger;
             _applicationSettings = applicationSettings;
+            Init();
         }
 
         public DateTime CurrentTime() => TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, GetLocalTimeZone());
 
         public TimeZoneInfo GetLocalTimeZone()
         {
-            return TimeZoneInfo.FindSystemTimeZoneById(_applicationSettings.TimeZone);
+            if (_localTimeZone == null)
+            {
+                throw new InvalidOperationException($"Time zone '{_applicationSettings.TimeZone}' not found or not initialized.");
+            }
+            return _localTimeZone;
         }
 
         public DateTime LocalToUtc(DateTime local) => TimeZoneInfo.ConvertTimeToUtc(local, GetLocalTimeZone());
@@ -34,6 +41,22 @@ namespace EnvironmentMonitor.Infrastructure.Services
         public string FormatDateTime(DateTime dateTime)
         {
             return dateTime.ToString("dd.MM.yyyy HH:mm:ss");
+        }
+
+        private void Init()
+        {
+            _logger.LogInformation($"Initializing DateService with time zone '{_applicationSettings.TimeZone}'");
+
+            if (TimeZoneInfo.TryFindSystemTimeZoneById(_applicationSettings.TimeZone, out TimeZoneInfo? timeZone))
+            {
+                _logger.LogInformation($"Time zone '{_applicationSettings.TimeZone}' found: {timeZone.DisplayName}");
+                _localTimeZone = timeZone;
+            }
+            else
+            {
+                _logger.LogWarning($"Time zone '{_applicationSettings.TimeZone}' not found");
+                _localTimeZone = null;
+            }
         }
     }
 }
