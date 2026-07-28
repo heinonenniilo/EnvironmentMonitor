527302a775f9510a325f3fc3e95dc432588eab26
heinonenniilo <niilo.heinonen@proton.me>
2026-07-28 23:39:17 +0300

    Migrations for Posgtre, add ExternalId

---
+++ src/EnvironmentMonitor.PostgreSqlMigrations/Migrations/Measurement/20260728202612_AddExternalIdDeviceMessages.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EnvironmentMonitor.PostgreSqlMigrations.Migrations.Measurement
+{
+    /// <inheritdoc />
+    public partial class AddExternalIdDeviceMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ExternalId",
+                table: "DeviceMessages",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsImported",
+                table: "DeviceMessages",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "StatusVariables",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Key = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Value = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    CreatedUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "LOCALTIMESTAMP"),
+                    UpdatedUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "LOCALTIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusVariables", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CommunicationChannels",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 3, "Sync", "Sync" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StatusVariables_Key",
+                table: "StatusVariables",
+                column: "Key",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StatusVariables");
+
+            migrationBuilder.DeleteData(
+                table: "CommunicationChannels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "DeviceMessages");
+
+            migrationBuilder.DropColumn(
+                name: "IsImported",
+                table: "DeviceMessages");
+        }
+    }
+}
